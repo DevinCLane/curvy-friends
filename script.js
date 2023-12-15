ef952b80fd0c4b97b2e91809688b8be29f24d22e
Devin Lane <devin.c.lane@gmail.com>
2023-12-14 19:18:35 -0800

    variable width, quadratic curves

--- script.js
+++ script.js
@@ -82,6 +82,7 @@ class Particle {
 
     draw() {
         ctx.beginPath();
+        ctx.lineWidth = this.size * 0.05;
         ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
         ctx.stroke();
     }
@@ -97,6 +98,24 @@ function handleParticles() {
     for (let i = 0; i < particleArray.length; i++) {
         particleArray[i].draw();
         particleArray[i].update();
+
+        for (let j = i + 1; j < particleArray.length; j++) {
+            const dx = particleArray[i].x - particleArray[j].x;
+            const dy = particleArray[i].y - particleArray[j].y;
+            const distance = Math.hypot(dx, dy);
+
+            if (distance < 200) {
+                ctx.beginPath();
+                ctx.moveTo(particleArray[i].x, particleArray[i].y);
+                ctx.quadraticCurveTo(
+                    particleArray[i].x + distance,
+                    particleArray[i].y + distance,
+                    particleArray[j].x,
+                    particleArray[j].y
+                );
+                ctx.stroke();
+            }
+        }
 
         if (particleArray[i].size <= 0.2) {
             particleArray.splice(i, 1);
